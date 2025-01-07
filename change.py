a1045966bdb523e8c85074e9b5e76df2660c24ad
xhrist14n <xhrist14n@gmail.com>
2025-01-07 00:51:38 +0000

    change

--- change.py
+++ change.py
@@ -2,15 +2,17 @@
 file_path = "build/index.html"
 
 # Open the file, read its content, and replace href="/"
-with open(file_path, "r") as file:
+with open(file_path, "r", encoding="utf-8") as file:
     content = file.read()
 
 # Replace href="/" with href="/reactone/"
-content = content.replace('href="/"', 'href="/reactone/"')
-content = content.replace('src="/"', 'src="/reactone/"')
+content = content.replace('href="/', 'href="/reactone/')
+content = content.replace('src="/', 'src="/reactone/')
+
+print(content)
 
 # Write the modified content back to the file
-with open(file_path, "w") as file:
+with open(file_path, "w", encoding="utf-8") as file:
     file.write(content)
 
 print(f"Updated {file_path}")
